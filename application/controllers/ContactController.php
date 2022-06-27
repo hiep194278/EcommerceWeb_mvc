383fd494de68bfca58c84e221e30cd3452ffac2d
vvt2001 <vuvietthang1508@gmail.com>
2022-06-27 18:16:27 +0700

    thêm footer và trang giới thiệu & liên hệ

---
+++ application/controllers/ContactController.php
@@ -0,0 +1,10 @@
+<?php
+require_once ROOT . DS . 'application' . DS . 'controllers' . DS . 'Controller.php';
+require_once ROOT . DS . 'application' . DS . 'controllers' . DS . 'BaseController.php';
+
+class ContactController extends BaseController implements Controller {
+    public function render() {
+      require_once ROOT . DS . 'application' . DS . 'views' . DS . 'contact.php'; 
+    }
+  }
+  
---
+++ application/views/contact.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Ecommerce</title>
+    <link rel="stylesheet" href="css/card.css" />    
+    <link rel="stylesheet" href="css/contact.css" />
+  </head>
+
+  <body>
+    <?php require_once ROOT . DS . 'application' . DS . 'views' . DS . 'header.php'; ?>
+ 
+    <br />
+
+    <h1 class="header">THÔNG TIN LIÊN HỆ ECOMMERCE</h1>
+    <p class="paragraph">Công ty Cổ phần đầu tư công nghệ ECOMMERCE là một trong những doanh nghiệp hàng đầu hoạt động trong lĩnh vực kinh doanh các sản phẩm CNTT. Công ty đã tạo được chỗ đứng vững chắc trên thị trường bán buôn, bán lẻ, trở thành thương hiệu quen thuộc và là đối tác tin cậy của nhiều bạn hàng trong nước và Quốc tế.</p>
+    <p class="paragraph">Luôn lấy yếu tố hài hòa về lợi ích làm nền tảng, lãnh đạo Công ty hiểu rằng, niềm tin của khách hàng về giá thành, chất lượng và dịch vụ là sự sống còn của Công ty. Do vậy, mọi hoạt động kinh doanh của Công ty luôn hướng tới mục tiêu tôn trọng và bảo đảm quyền lợi cho khách hàng, chinh phục khách hàng bằng chất lượng sản phẩm và dịch vụ tối ưu. Để được phục vụ tốt nhất, Quý khách vui lòng liên hệ với chúng tôi theo các thông tin sau:</p>
+    
+    <div class="contact-card-line">
+        <div class="contact-card-box">
+            <h2 class= "header-box"> LIÊN HỆ CHÀO HÀNG, HỢP TÁC KINH DOANH</h2>
+            <p>– Phụ trách: <span style="font-weight: 500">Đàm Công Tuấn</span></p>
+            <p>– Email: <span style="font-weight: 500">tuandc@hacom.vn</span></p>
+            <p>– Mobile: <span style="font-weight: 500">0933 735 688</span></p>
+        </div>
+        <div class="contact-card-box">
+            <h2 class= "header-box"> PHÒNG KINH DOANH ONLINE</h2>
+            <p>– Phụ trách: <span style="font-weight: 500">Trịnh Hoàng Tùng</span></p>
+            <p>– Email: <span style="font-weight: 500">tungth@hacom.vn</span></p>
+            <p>– Mobile: <span style="font-weight: 500">0936 027 037</span></p>
+        </div>
+    </div>
+
+    <div class="contact-card-line">
+        <div class="contact-card-box">
+            <h2 class= "header-box"> PHÒNG KẾ HOẠCH KINH DOANH</h2>
+            <p>– Phụ trách: <span style="font-weight: 500">Hoàng Quốc Hiếu</span></p>
+            <p>– Email: <span style="font-weight: 500">hieuhq@hacom.vn</span></p>
+            <p>– Mobile: <span style="font-weight: 500">093 6493 126</span></p>
+        </div>
+        <div class="contact-card-box">
+            <h2 class= "header-box"> PHÒNG DỰ ÁN</h2>
+            <p>– Phụ trách: <span style="font-weight: 500">Nguyễn Văn Hiển</span></p>
+            <p>– Email: <span style="font-weight: 500">hiennv@hacom.vn</span></p>
+            <p>– Mobile: <span style="font-weight: 500">096 411 0606</span></p>
+        </div>
+    </div>
+
+  </body>
+
+  
+</html>
